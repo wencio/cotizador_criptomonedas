de39da287836b82932682f6a5bb7bbd9a93303ed
wencio <wdannimartinez@gmail.com>
2022-03-01 21:13:05 -0500

    Cryptocurrency quoter using react and styled components
    Cryptocurrency quoter using react and styled components

---
+++ src/data/monedas.js
@@ -0,0 +1,13 @@
+const monedas =[
+    {id: 'USD', nombre : 'Dolar de Estados Unidos'},
+    {id: 'MXN', nombre : 'Peso Mexicano'},
+    {id: 'Euro', nombre : 'Euro'},
+    {id: 'GBP', nombre : 'Libra Esterlina'},
+    {id: 'COP', nombre : 'Peso Colombiano'}
+   
+
+]
+
+export {
+    monedas
+}
